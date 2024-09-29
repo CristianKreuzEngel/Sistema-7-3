31259f767c00bfbe3491b08464023a60f3c23fe3
Cristian Kreuz Engel <kreuzengelc@gmail.com>
2024-09-28 22:43:08 -0300

    Introduçao ao frontend

---
+++ sistema_73-front/src/pages/LoginPage.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { ILogin } from 'components/models';
+
+export default defineComponent({
+  name: 'LoginPage',
+  data: () => {
+    return {
+      form: {
+        username: '',
+        password: ''
+      } as ILogin,
+    };
+  }
+});
+</script>
+
+<template>
+  <div class="q-pa-md flex flex-center" style="height: 100vh;">
+    <q-card
+      class="my-card row items-center justify-evenly"
+      square
+      bordered
+      flat
+    >
+      <q-form
+        class="q-gutter-md"
+      >
+      <q-input
+        outlined
+        label="Username"
+        v-model="form.username"
+        type="text"
+      />
+      <q-input
+        outlined
+        label="Password"
+        v-model="form.password"
+        type="password"
+      />
+      <q-btn
+        label="Entrar"
+        color="primary"
+        type="submit"
+      />
+      </q-form>
+    </q-card>
+  </div>
+</template>
+
+<style scoped>
+.my-card {
+  width: 100%;
+  max-width: 400px;
+  height: 100%;
+  max-height: 400px;
+}
+</style>
---
+++ sistema_73-front/src/router/routes.ts
@@ -0,0 +1,19 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+  {
+    path: '/login',
+    component: () => import('pages/LoginPage.vue'),
+  },
+];
+
+export default routes;
